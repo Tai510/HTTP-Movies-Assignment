bcfca20d7c1ffd4224ad869c65fc3b93f1bd52d0
tashi woeser <tashiwoeser@tashis-MacBook-Pro.local>
2019-10-17 14:25:32 -0700

    updating form

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,6 +1,9 @@
 import React from "react";
 import axios from "axios";
 import MovieCard from "./MovieCard";
+import { NavLink , Route} from 'react-router-dom'
+
+
 export default class Movie extends React.Component {
   constructor(props) {
     super(props);
@@ -39,13 +42,17 @@ export default class Movie extends React.Component {
     return (
       <div className="save-wrapper">
         <MovieCard movie={this.state.movie} />
-        <div className="save-button" onClick={this.saveMovie}>
+        <button className="save-button" onClick={this.saveMovie}>
           Save
-        </div>
-        <div className='edit-button'>
-            <button>Edit</button>
-            <button>Delete</button>
-        </div>
+        </button>
+        <NavLink to={`/update-movie/${this.state.movie.id}`}>
+        <button className="update-button">
+          Update
+        </button>
+        </NavLink>
+        <button className="delete-button">
+          Delete
+        </button>
       </div>
     );
   }
--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -1,30 +1,78 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import axios from 'axios';
 
-
-const initialValue = {
-    title: '',
-    director: '',
-    metascore: '',
-    stars: []
-}
-
 const UpdateForm = props => {
-    const [item, setItem] = useState(initialValue)
+    const initialItem = {
+        id: props.match.params.id,
+        title: "",
+        director: "",
+        metascore: "",
+    };
 
-    useEffect(() => {
-        const id = props.match.params.id;
-        const updateMovie = props.items.find(item => {
-            `${item.id}` === id
-        })
-        if(updateMovie) {
-            setItem(updateMovie);
-        }
-    }, [])
+    const [stars, setStars] =useState([]);
+    const [update, setUpdate] = useState(initialItem);
+    console.log('update',update)
 
-    return (
-        <div>
+  const changeHandler = e => {
+      e.preventDefault();
+      setUpdate({...update, [e.target.name]: e.target.value })
+  }
 
-        </div>
-    )
+  const changeHandlerStars = e => {
+    e.preventDefault();
+    setStars({...stars, [e.target.name]: [e.target.value] })
 }
+
+const data={
+    ...update,
+    ...stars
+}
+console.log('data', data)
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    axios
+    .put(`http://localhost:5000/api/movies/${props.match.params.id}`, data )
+    .catch(err => {
+        console.log(err.response)
+    })
+    props.history.push('/')
+    window.location.href = window.location.href
+  }
+
+  return (
+    <div>
+      <h2>Update Item</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="title"
+          onChange={changeHandler}
+          value={update.title}
+        />
+        <input
+          type="number"
+          name="metascore"
+          onChange={changeHandler}
+          value={update.metascore}
+        />
+        <input
+          type='text'
+          name="director"
+          onChange={changeHandler}
+          value={update.director}
+        />
+        <input
+          type="array"
+          name="stars"
+          onChange={changeHandlerStars}
+          value={update.stars}
+        />
+        <button className="form-button">Update Movie</button>
+      </form>
+    </div>
+  );
+}
+
+
+export default UpdateForm;
